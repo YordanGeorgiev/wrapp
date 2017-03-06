999a0c165d5f5b3c7c9647122e6afab5be671640
YordanGeorgiev <yordan.georgiev@gmail.com>
2017-03-06 13:29:49 +0200

    implemented generic testing report creation for actions ...

--- src/bash/wrapp/tests/pckg/create-relative-package.test.sh
+++ src/bash/wrapp/tests/pckg/create-relative-package.test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# v1.1.0
+# v1.1.4
 #------------------------------------------------------------------------------
 # creates a package from the relative file paths specified in the .dev file
 #------------------------------------------------------------------------------
@@ -11,22 +11,36 @@ doTestCreateRelativePackage(){
 	doSpecCreateRelativePackage
 
 	doHelpCreateRelativePackage
-	
+   
+   #set -x
 	# test the call with the include file of the current env - usually dev
+   exit_code=0
 	bash src/bash/wrapp/wrapp.sh -a create-relative-package
-	test -z "$sleep_interval" || sleep "$sleep_interval"
+   export exit_code=$?
+	doLog " create-relative-package.test-1 exit_code: $exit_code "
+   test -z "$sleep_interval" || sleep "$sleep_interval"
+   test $exit_code -ne 0 && return
 	
 	# test the call with the include file of the tst env
-	bash src/bash/wrapp/wrapp.sh -a create-relative-package -i met/.tst.wrapp
-	test -z "$sleep_interval" || sleep "$sleep_interval"
+   bash src/bash/wrapp/wrapp.sh -a create-relative-package -i met/.tst.wrapp
+   export exit_code=$?
+	doLog " create-relative-package.test-2 exit_code: $exit_code "
+   test -z "$sleep_interval" || sleep "$sleep_interval"
+   test $exit_code -ne 0 && return
 	
 	# test the call with the include file of the prd env
 	bash src/bash/wrapp/wrapp.sh -a create-relative-package -i met/.prd.wrapp
-	test -z "$sleep_interval" || sleep "$sleep_interval"
+   export exit_code=$?
+	doLog " create-relative-package.test-3 exit_code: $exit_code "
+   test -z "$sleep_interval" || sleep "$sleep_interval"
+   test $exit_code -ne 0 && return
 	
-	# test the call with the include file of the prd env
-	bash src/bash/wrapp/wrapp.sh -a create-relative-package -i met/.git.wrapp
-	test -z "$sleep_interval" || sleep "$sleep_interval"
+   # test the call with the include file of the prd env
+   bash src/bash/wrapp/wrapp.sh -a create-relative-package -i met/.git.wrapp
+   export exit_code=$?
+	doLog " create-relative-package.test-4 exit_code: $exit_code "
+   test -z "$sleep_interval" || sleep "$sleep_interval"
+   test $exit_code -ne 0 && return
 	
 	doLog " STOP  : create-relative-package.test"
 }
